3a7c4835828f84fbb920bf832267432579fd7cdb
Mohamed ELFallah <eng-mohamed.elflah1621@alexu.edu.eg>
2021-09-14 08:41:31 +0200

    يارب

--- currency-converter/src/CurrencyRow.js
+++ currency-converter/src/CurrencyRow.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function CurrencyRow(props) {
+export default function CurrencyRow(data) {
   const {
     currencyOptions,
     selectedCurrency,
     onChangeCurrency,
     onChangeAmount,
     amount
-  } = props
+  } = data
   return (
     <div>
       <input type="number" className="input" value={amount} onChange={onChangeAmount} />
---
+++ currency-converter/src/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => (
+    <div className="footer">
+        
+         <p>
+          developers:<br/>
+           Gehad Essam Zayed <a  href="https://www.linkedin.com/in/gehad-essam-ba06bb211/">contact</a><br/>
+           Mohamed Ahmed AbdElHameed ElFallah <a  href="https://www.linkedin.com/in/m-a-elfallah/">contact</a>
+           </p>
+        
+    </div>
+  
+
+);
+export default Footer;
